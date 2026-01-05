a5dab301b69c78c3fde0f1c9cc2a8a180c84f4eb
Munif Tanjim <hello@muniftanjim.dev>
2026-01-06 00:13:00 +0600

    feat(reader): support seek for encrypted archive

--- decrypt_reader.go
+++ decrypt_reader.go
@@ -4,6 +4,7 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"io"
+	"io/fs"
 )
 
 // cipherBlockReader implements Block Mode decryption of an io.Reader object.
@@ -156,10 +157,134 @@ func (cr *cipherBlockFileReader) writeToN(w io.Writer, n int64) (int64, error) {
 	return cr.cbr.writeToN(w, n)
 }
 
-func newAesDecryptFileReader(r archiveFile, key, iv []byte) (*cipherBlockFileReader, error) {
+func newAesDecryptFileReader(r archiveFile, key, iv []byte) (archiveFile, error) {
+	if sr, ok := r.(archiveFileSeeker); ok {
+		return newAesDecryptFileReadSeeker(sr, key, iv)
+	}
 	cbr, err := newAesDecryptReader(r, key, iv)
 	if err != nil {
 		return nil, err
 	}
 	return &cipherBlockFileReader{archiveFile: r, cbr: cbr}, nil
+}
+
+// cipherBlockFileReadSeeker is a seekable variant of cipherBlockFileReader.
+// It implements io.Seeker by re-reading and decrypting from the start.
+type cipherBlockFileReadSeeker struct {
+	archiveFileSeeker
+	cbr    *cipherBlockReader
+	key    []byte
+	iv     []byte
+	offset int64 // current decrypted byte position
+	size   int64 // total encrypted data size (decrypted size is the same)
+}
+
+func (cr *cipherBlockFileReadSeeker) ReadByte() (byte, error) {
+	b, err := cr.cbr.ReadByte()
+	if err == nil {
+		cr.offset++
+	}
+	return b, err
+}
+
+func (cr *cipherBlockFileReadSeeker) Read(p []byte) (int, error) {
+	n, err := cr.cbr.Read(p)
+	cr.offset += int64(n)
+	return n, err
+}
+
+func (cr *cipherBlockFileReadSeeker) WriteTo(w io.Writer) (int64, error) {
+	n, err := cr.cbr.WriteTo(w)
+	cr.offset += n
+	return n, err
+}
+
+func (cr *cipherBlockFileReadSeeker) writeToN(w io.Writer, n int64) (int64, error) {
+	m, err := cr.cbr.writeToN(w, n)
+	cr.offset += m
+	return m, err
+}
+
+func (cr *cipherBlockFileReadSeeker) Seek(offset int64, whence int) (int64, error) {
+	// Calculate absolute offset
+	switch whence {
+	case io.SeekStart:
+		// offset is already absolute
+	case io.SeekCurrent:
+		offset += cr.offset
+	case io.SeekEnd:
+		offset += cr.size
+	default:
+		return 0, fs.ErrInvalid
+	}
+	if offset < 0 {
+		return 0, fs.ErrInvalid
+	}
+
+	// O(1) seek using CBC IV reconstruction trick:
+	// To decrypt block N, we only need Ciphertext[N-1] as the IV.
+	// This avoids decrypting all previous blocks.
+	const blockSize = 16 // AES block size
+	blockIndex := offset / int64(blockSize)
+	blockOffset := int(offset % int64(blockSize))
+
+	var iv []byte
+	var err error
+
+	if blockIndex > 0 {
+		// Read previous ciphertext block as IV
+		cipherPos := (blockIndex - 1) * int64(blockSize)
+		_, err = cr.archiveFileSeeker.Seek(cipherPos, io.SeekStart)
+		if err != nil {
+			return 0, err
+		}
+		iv = make([]byte, blockSize)
+		_, err = io.ReadFull(cr.archiveFileSeeker, iv)
+		if err != nil {
+			return 0, err
+		}
+		// Stream is now positioned at start of target block
+	} else {
+		// For block 0, use original IV
+		iv = cr.iv
+		_, err = cr.archiveFileSeeker.Seek(0, io.SeekStart)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	// Recreate cipher with reconstructed IV
+	cr.cbr, err = newAesDecryptReader(cr.archiveFileSeeker, cr.key, iv)
+	if err != nil {
+		return 0, err
+	}
+
+	// Handle mid-block seek by discarding initial bytes of target block
+	if blockOffset > 0 {
+		discard := make([]byte, blockOffset)
+		_, err = io.ReadFull(cr.cbr, discard)
+		if err != nil {
+			return 0, err
+		}
+	}
+
+	cr.offset = offset
+	return offset, nil
+}
+
+func newAesDecryptFileReadSeeker(r archiveFileSeeker, key, iv []byte) (*cipherBlockFileReadSeeker, error) {
+	cbr, err := newAesDecryptReader(r, key, iv)
+	if err != nil {
+		return nil, err
+	}
+	// Get the size from the current file header
+	h := r.currFile()
+	size := h.PackedSize
+	return &cipherBlockFileReadSeeker{
+		archiveFileSeeker: r,
+		cbr:               cbr,
+		key:               key,
+		iv:                iv,
+		size:              size,
+	}, nil
 }
